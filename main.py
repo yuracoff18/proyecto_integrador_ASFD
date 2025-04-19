59f84678596effa50db5a91463237e6a8aa8280d
Yura <yurayt18@gmail.com>
2025-04-19 19:45:12 +0000

    Esqueleto

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from src import funciones
+
+
+def main():
+    
+    st.title("Inicio de sesion")
+    
+    with st.form("Inicio de sesion"):
+        usuario = st.text_input("",placeholder="Usuario")
+        contraseña = st.text_input("", placeholder="Contraseña")
+
+        login_button = st.form_submit_button("Iniciar sesion")
+    
+        register_button = st.form_submit_button("Registrar usuario")
+        
+
+    if register_button:
+        st.switch_page("pages/register.py")
+
+    if login_button:
+        funciones.login(usuario, contraseña)
+
+
+if __name__ == "__main__":
+    main()
